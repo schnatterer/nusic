140a7779957e7289b36aea5b39b89e0f79f6b9d7
schnatterer <johannes@schnatterer.info>
2015-06-10 22:12:31 +0200

    Added new license headers

--- nusic-core-android/src/main/java/info/schnatterer/nusic/core/i18n/CoreMessageKey.java
+++ nusic-core-android/src/main/java/info/schnatterer/nusic/core/i18n/CoreMessageKey.java
@@ -1,24 +1,24 @@
-/* Copyright (C) 2015 Johannes Schnatterer
- * 
+/**
+ * ﻿Copyright (C) 2013 Johannes Schnatterer
+ *
  * See the NOTICE file distributed with this work for additional
  * information regarding copyright ownership.
- *  
- * This file is part of nusic.
- * 
- * nusic is free software: you can redistribute it and/or modify
+ *
+ * This file is part of nusic-core-android.
+ *
+ * nusic-core-android is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * nusic is distributed in the hope that it will be useful,
+ * nusic-core-android is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
- * along with nusic.  If not, see <http://www.gnu.org/licenses/>.
+ * along with nusic-core-android.  If not, see <http://www.gnu.org/licenses/>.
  */
- 
 package info.schnatterer.nusic.core.i18n;
 
 import java.util.Locale;
