057ff211548219344b1e95ddfe5beb4f1d314067
schnatterer <johannes@schnatterer.info>
2016-09-04 17:39:06 +0200

    Refactored Logs.setLogCatLevel() for better maintainability

--- nusic-ui-android/src/main/java/info/schnatterer/nusic/android/util/Logs.java
+++ nusic-ui-android/src/main/java/info/schnatterer/nusic/android/util/Logs.java
@@ -1,21 +1,21 @@
 /**
  * Copyright (C) 2013 Johannes Schnatterer
- *
+ * <p/>
  * See the NOTICE file distributed with this work for additional
  * information regarding copyright ownership.
- *
+ * <p/>
  * This file is part of nusic.
- *
+ * <p/>
  * nusic is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * <p/>
  * nusic is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * <p/>
  * You should have received a copy of the GNU General Public License
  * along with nusic.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -27,8 +27,10 @@ import java.util.Collections;
 import java.util.List;
 
 import org.slf4j.LoggerFactory;
+import org.slf4j.Marker;
 
 import android.content.Context;
+
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.android.LogcatAppender;
@@ -38,10 +40,11 @@ import ch.qos.logback.core.filter.Filter;
 
 /**
  * Basic abstraction of the log mechanism (SLF4J + logback-android) used here.
- *
  */
 public final class Logs {
-    /** Name of the logcat logger, as configured in logback.xml */
+    /**
+     * Name of the logcat logger, as configured in logback.xml
+     */
     static final String LOGCAT_LOGGER_NAME = "LOGCAT";
 
     /**
@@ -58,12 +61,11 @@ public final class Logs {
      * Set the log level of the root logger. <b>Note</b>: This depends on the
      * actual logging framework.
      *
-     * @param level
-     *            the log level to set
+     * @param level the log level to set
      */
     public static void setRootLogLevel(String level) {
         Logger root = (Logger) LoggerFactory
-                .getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME);
+            .getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME);
         root.info("root.getLevel(): {}", root.getLevel().toString());
         root.info("Setting level to {}", level);
         root.setLevel(Level.toLevel(level));
@@ -72,9 +74,7 @@ public final class Logs {
     /**
      * Returns the current log file.
      *
-     * @param context
-     *            context to get the app-private files from
-     *
+     * @param context context to get the app-private files from
      * @return the current log file
      */
     public static File findNewestLogFile(Context context) {
@@ -82,29 +82,9 @@ public final class Logs {
     }
 
     /**
-     * Sort array of files descending by name and returns the first one. For
-     * file names like <code>2015-06-25.log</code> and
-     * <code>2015-06-26.log</code> this returns the newest log file, for example
-     * <code>2015-06-26.log</code>.
-     *
-     * @param logFiles
-     *            the array of log files
-     * @return the newest log file or <code>null</code> if <code>logFiles</code> is <code>null</code>
-     */
-    static File findNewestLogFile(File[] logFiles) {
-        // Sort descendingly
-        if (logFiles == null) {
-            return null;
-        }
-        Arrays.sort(logFiles, Collections.reverseOrder());
-        return logFiles[0];
-    }
-
-    /**
      * Returns all log files.
      *
-     * @param context
-     *            context to get the app-private files from
+     * @param context context to get the app-private files from
      * @return all log files
      */
     public static File[] getLogFiles(Context context) {
@@ -114,8 +94,7 @@ public final class Logs {
     /**
      * Returns the folder where log files are stored.
      *
-     * @param context
-     *            context to get the app-private files from
+     * @param context context to get the app-private files from
      * @return the directory that contains log files
      */
     public static File getLogFileDirectory(Context context) {
@@ -126,31 +105,47 @@ public final class Logs {
      * Sets the threshold log level for the logCat appender. Note: Must be >=
      * Root Log Level. If not, a warning is toasted.
      *
-     * @param logLevelLogCat
-     *            the threshold level to set for logCat appender
-     * @param context
-     *            (optional) context needed for toasting warnings. If
-     *            <code>null</code> no toast is displayed
+     * @param logLevelLogCat the threshold level to set for logCat appender
+     * @param context        (optional) context needed for toasting warnings. If
+     *                       <code>null</code> no toast is displayed
      */
     public static void setLogCatLevel(String logLevelLogCat, Context context) {
         /* Find appender */
         Logger root = (Logger) LoggerFactory
-                .getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME);
+            .getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME);
         LogcatAppender logcatAppender = (LogcatAppender) root
-                .getAppender(LOGCAT_LOGGER_NAME);
+            .getAppender(LOGCAT_LOGGER_NAME);
         if (logcatAppender == null) {
-            String message = "No appender \"" + LOGCAT_LOGGER_NAME
-                    + "\" configured. Can't change threshold";
-            root.warn(message);
-            if (context != null) {
-                Toast.toast(context, message);
-            }
+            warnAndToast(context, root,
+                "No appender \"" + LOGCAT_LOGGER_NAME + "\" configured. Can't change threshold");
             return;
         }
 
+        setLogCatThresholdFilter(logLevelLogCat, logcatAppender, root);
+
+        /* Result */
+        root.info("Setting logcat level to {}. root.getLevel(): {}",
+            logLevelLogCat, root.getLevel().toString());
+
+        if (greaterThan(root.getLevel(), Level.toLevel(logLevelLogCat))) {
+            warnAndToast(context, root,
+                String.format("Root level(%s) > logcat level (%s)!", root.getLevel(), logLevelLogCat)
+            );
+        }
+    }
+
+    /**
+     * Actually sets the logCat level. This in realized using a {@link ThresholdFilter}.
+     *
+     * @param logLevelLogCat the level to set to the threshold filter
+     * @param logcatAppender logcat appender to change
+     * @param root           root logger, needed as context and for logging changes
+     */
+    private static void setLogCatThresholdFilter(String logLevelLogCat,
+                                                 LogcatAppender logcatAppender, Logger root) {
         /* Find and change filter */
         List<Filter<ILoggingEvent>> filters = logcatAppender
-                .getCopyOfAttachedFiltersList();
+            .getCopyOfAttachedFiltersList();
         ThresholdFilter threshold = null;
         for (Filter<ILoggingEvent> filter : filters) {
             if (filter instanceof ThresholdFilter) {
@@ -172,17 +167,41 @@ public final class Logs {
         for (Filter<ILoggingEvent> filter : filters) {
             logcatAppender.addFilter(filter);
         }
+    }
 
-        /* Result */
-        root.info("Setting logcat level to {}. root.getLevel(): {}",
-                logLevelLogCat, root.getLevel().toString());
-        if (root.getLevel().toInt() > Level.toLevel(logLevelLogCat).toInt()) {
-            String message = "Root level(" + root.getLevel().toString()
-                    + ") > logcat level (" + logLevelLogCat + ")";
-            root.warn(message);
-            if (context != null) {
-                Toast.toast(context, message);
-            }
+    /**
+     * @return <code>true</code> if <code>a</code> greater than <code>b</code>. Otherwise <code>false</code>
+     */
+    private static boolean greaterThan(Level a, Level b) {
+        return a.toInt() > b.toInt();
+    }
+
+    /**
+     * Writes message to {@link Logger#warn(Marker, String, Object)} and
+     * {@link android.widget.Toast}.
+     */
+    private static void warnAndToast(Context context, Logger logger, String message) {
+        logger.warn(message);
+        if (context != null) {
+            android.widget.Toast.makeText(context, message, android.widget.Toast.LENGTH_LONG).show();
         }
+    }
+
+    /**
+     * Sort array of files descending by name and returns the first one. For
+     * file names like <code>2015-06-25.log</code> and
+     * <code>2015-06-26.log</code> this returns the newest log file, for example
+     * <code>2015-06-26.log</code>.
+     *
+     * @param logFiles the array of log files
+     * @return the newest log file or <code>null</code> if <code>logFiles</code> is <code>null</code>
+     */
+    static File findNewestLogFile(File[] logFiles) {
+        // Sort descendingly
+        if (logFiles == null) {
+            return null;
+        }
+        Arrays.sort(logFiles, Collections.reverseOrder());
+        return logFiles[0];
     }
 }
